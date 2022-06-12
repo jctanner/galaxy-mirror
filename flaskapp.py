7b6934b45d744c4a820c6a77ce03c319ed211c48
James Tanner <tanner.jc@gmail.com>
2022-06-12 15:49:33 -0400

    poor mans caching

--- flaskapp.py
+++ flaskapp.py
@@ -1,18 +1,46 @@
 #!/usr/bin/env python
 
+import json
+import hashlib
+import os
+
 from flask import jsonify
 from flask import Flask
 from flask import redirect
 from flask import request
 
 import requests
-import requests_cache
+#import requests_cache
 
 
 app = Flask(__name__)
 
 BASEURL = 'https://galaxy.ansible.com'
-requests_cache.install_cache('/data/demo_cache')
+CACHEDIR = '/data'
+#requests_cache.install_cache('/data/demo_cache')
+
+
+def get_cached_url(url):
+    ho = hashlib.sha256(url.encode('utf-8'))
+    hd = ho.hexdigest()
+    cf = f'{CACHEDIR}/{hd}.json'
+
+    if not os.path.exists(CACHEDIR):
+        os.makedirs(CACHEDIR)
+
+    if not os.path.exists(cf):
+        app.logger.info(f'CACHE MISS {url}')
+
+        rr = requests.get(url)
+        ds = rr.json()
+        with open(cf, 'w') as f:
+            f.write(json.dumps({'url': url, 'data': ds}, indent=2))
+
+        return ds
+
+    app.logger.info(f'CACHE HIT {url}')
+    with open(cf, 'r') as f:
+        return json.loads(f.read())['data']
 
 
 @app.route('/')
@@ -23,9 +51,10 @@ def root():
 @app.route('/api/')
 def api_root():
     this_url = BASEURL + '/api/'
-    print(f'GET {this_url}')
-    rr = requests.get(this_url)
-    return jsonify(rr.json())
+    #print(f'GET {this_url}')
+    #rr = requests.get(this_url)
+    #return jsonify(rr.json())
+    return get_cached_url(this_url)
 
 
 @app.route('/api/<path:api_path>')
@@ -35,10 +64,11 @@ def api_abstract_path(api_path):
         this_url += '?'
         for k,v in dict(request.args).items():
             this_url += f'{k}={v}'
-    print(f'GET {this_url}')
-    rr = requests.get(this_url)
-    return jsonify(rr.json())
+    #print(f'GET {this_url}')
+    #rr = requests.get(this_url)
+    #return jsonify(rr.json())
+    return get_cached_url(this_url)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=80, debug=True)
